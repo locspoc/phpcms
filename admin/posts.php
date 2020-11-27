72901f8b9bf5a300ecdd49f5bf1cc414078fe180
locspoc <mrlocspoc@gmail.com>
2020-11-27 22:56:11 +1030

    110. Including Pages based on condition technique complete

--- admin/posts.php
+++ admin/posts.php
@@ -17,74 +17,43 @@
                             <small>Author</small>
                         </h1>
 
-                        <table class="table table-bordered table-hover">
+                        <?php 
 
+                        if(isset($_GET['source'])){
 
-                          <thead>
-                            <tr>
-                              <th>Id</th>
-                              <th>Author</th>
-                              <th>Title</th>
-                              <th>Category</th>
-                              <th>Status</th>
-                              <th>Image</th>
-                              <th>Tags</th>
-                              <th>Comments</th>
-                              <th>Date</th>
-                            </tr>
-                          </thead>
+                          $source = $_GET['source'];
 
-                          <tbody>
+                        } else {
 
-                            <?php 
-                            
-                            $query = "SELECT * FROM posts";
-                            $select_posts = mysqli_query($connection, $query);
-                                  
-                            while($row = mysqli_fetch_assoc($select_posts)) {
-                            $post_id = $row['post_id'];
-                            $post_author = $row['post_author'];
-                            $post_title = $row['post_title'];
-                            $post_category_id = $row['post_category_id'];
-                            $post_status = $row['post_status'];
-                            $post_image = $row['post_image'];
-                            $post_tags = $row['post_tags'];
-                            $post_comment_count = $row['post_comment_count'];
-                            echo $post_date = $row['post_date'];
+                          $source = '';
 
-                            echo "<tr>";
-                            echo "<td>{$post_id}</td>";
-                            echo "<td>{$post_author}</td>";
-                            echo "<td>{$post_title}</td>";
-                            echo "<td>{$post_category_id}</td>";
-                            echo "<td>{$post_status}</td>";
-                            echo "<td><img src='../images/{$post_image}' alt='image' class='img-responsive'></td>";
-                            echo "<td>{$post_tags}</td>";
-                            echo "<td>{$post_comment_count}</td>";
-                            echo "<td>{$post_date}</td>";
-                            echo "</tr>";
-                            
+                        }
 
-                            }
-                            
-                            ?>
+                        switch($source) {
 
+                        case '34';
+                          echo "NICE 34";
+                        break;
+                        
+                        case '100';
+                          echo "NICE 100";
+                        break;
+                        
+                        case '200';
+                        echo "NICE 200";
+                        break;
 
-                              <td>10</td>
-                              <td>Loc Tran</td>
-                              <td>Bootstrap Framework</td>
-                              <td>Bootstrap</td>
-                              <td>Status</td>
-                              <td>Image</td>
-                              <td>Tags</td>
-                              <td>Comments</td>
-                              <td>Date</td>
+                        default: 
 
-                            
+                        include "includes/view_all_posts.php";
 
-                          </tbody>
+                        break;
 
-                        </table>
+                        // Code here
+
+                        }
+
+                        ?>
 
 
                         
