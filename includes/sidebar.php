30dcee91120713839dacfb525c0c4e8d48246a3d
locspoc <mrlocspoc@gmail.com>
2021-03-29 22:17:57 +1030

    152. Creating the login form complete

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -18,7 +18,28 @@
                     <!-- /.input-group -->
                 </div>
                 
-                
+                <!-- Login -->
+                <div class="well">
+                    <h4>Login</h4>
+                    
+                    <form action="includes/login.php" method="POST">
+                    
+                    <div class="form-group">
+                        <input name="username" type="text" class="form-control" placeholder="Enter Username">
+                        
+                    </div>
+
+                    <div class="input-group">
+                        <input name="password" type="password" class="form-control" placeholder="Enter Password">
+                        <span class="input-group-btn">
+                            <button class="btn btn-primary" name="login" type="submit">Submit</button>
+
+                        </span>    
+                        
+                    </div>
+                    </form><!-- search from -->
+                    <!-- /.input-group -->
+                </div>
                 
                 
                 
