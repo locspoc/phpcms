0a080ff4025949ebfdd31e1c142d58cb81bb5ebf
locspoc <mrlocspoc@gmail.com>
2021-04-05 14:31:17 +0930

    Section 20: Improving Our CMS complete

--- index.php
+++ index.php
@@ -29,13 +29,7 @@
                         $post_content = substr($row['post_content'],0,100);
                         $post_status = $row['post_status'];
 
-                        if($post_status !== 'published') {
-
-                            echo "<h1 class='text-center'> NO POST SORRY </h1>";
-
-                        } else {
-
-                        
+                        if($post_status == 'published') {
                         
                         ?>
                         
@@ -60,7 +54,7 @@
 
                 <hr>  
                     
-                <?php }  } ?>
+                <?php } }?>
 
             </div>
 
