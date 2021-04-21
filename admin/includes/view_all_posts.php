83051736a94a85096d57851c24104ae448f47023
locspoc <mrlocspoc@gmail.com>
2021-04-21 15:57:16 +0930

    185. Adding Bulk Options Posts Part 6 complete

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -12,22 +12,94 @@ header("Location: posts.php"); exit;
 
 ?>
 
+<?php
+
+if(isset($_POST['checkBoxArray'])) {
+
+    foreach($_POST['checkBoxArray'] as $postValueId ){
+
+        $bulk_options = $_POST['bulk_options'];
+
+        switch($bulk_options) {
+            
+            case 'published':
+
+                $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id={$postValueId} ";
+
+                $update_to_published_status = mysqli_query($connection,$query);
+
+                confirmQuery($update_to_published_status);
+
+                break;
+            
+            case 'draft':
+
+                $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id={$postValueId} ";
+    
+                $update_to_draft_status = mysqli_query($connection,$query);
+    
+                confirmQuery($update_to_draft_status);
+    
+                break;
+
+            case 'delete':
+
+                $query = "DELETE FROM posts WHERE post_id={$postValueId} ";
+        
+                $update_to_delete_status = mysqli_query($connection,$query);
+        
+                confirmQuery($update_to_delete_status);
+        
+                break;
+
+        }
+
+    }
+
+}
+
+?>
+
+<form action="" method='POST'>
+
 <table class="table table-bordered table-hover">
+
+<div id="bulkOptionsContainer" class="col-xs-4">
+
+    <select class="form-control" name="bulk_options" id="bulk_options">
+    
+        <option value="">Select Option</option>
+        <option value="published">Publish</option>
+        <option value="draft">Draft</option>
+        <option value="delete">Delete</option>
+
+    </select>
+
+</div>
+
+<div class="col-xs-4">
+
+    <input type="submit" name="submit" class="btn btn-success" value="Apply">
+
+    <a class="btn btn-primary" href="add_post.php">Add New</a>
+
+</div>
 
 
                           <thead>
                             <tr>
-                              <th>Id</th>
-                              <th>Author</th>
-                              <th>Title</th>
-                              <th>Category</th>
-                              <th>Status</th>
-                              <th>Image</th>
-                              <th>Tags</th>
-                              <th>Comments</th>
-                              <th>Date</th>
-                              <th>Edit</th>
-                              <th>Delete</th>
+                                <th><input id="selectAllBoxes" type="checkbox"></th>
+                                <th>Id</th>
+                                <th>Author</th>
+                                <th>Title</th>
+                                <th>Category</th>
+                                <th>Status</th>
+                                <th>Image</th>
+                                <th>Tags</th>
+                                <th>Comments</th>
+                                <th>Date</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
                             </tr>
                           </thead>
 
@@ -50,6 +122,13 @@ header("Location: posts.php"); exit;
                             $post_date = $row['post_date'];
 
                             echo "<tr>";
+
+                            ?>
+
+                                <td><input class='checkBoxes' type='checkbox' name='checkBoxArray[]' value='<?php echo $post_id ?>'></td>
+
+                            <?php
+                            
                             echo "<td>{$post_id}</td>";
                             echo "<td>{$post_author}</td>";
                             echo "<td>{$post_title}</td>";
@@ -87,3 +166,5 @@ header("Location: posts.php"); exit;
                           </tbody>
 
                         </table>
+
+</form>
