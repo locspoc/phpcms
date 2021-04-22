3b661675ac2aaf00a4b99a32e87fe5f921f29f42
locspoc <mrlocspoc@gmail.com>
2021-04-22 19:50:27 +0930

    186. Adding Dropdown Option For Add Post Page complete

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -77,10 +77,14 @@
   </div>
 
   <div class="form-group">
-  
-    <label for="post_status">Post Status</label>
 
-      <input type="text" class="form-control" name="post_status">
+    <select name="post_status" id="">
+    
+      <option value="draft">Post Status</option>
+      <option value="published">Published</option>
+      <option value="draft">Draft</option>
+
+    </select>
   
   </div>
 
