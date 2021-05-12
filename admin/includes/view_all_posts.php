262476bec4694a11367286a237b5c5b6fa1198bb
locspoc <mrlocspoc@gmail.com>
2021-05-12 22:59:32 +0930

    231. Creating a link in comment count and sending a GET Request complete

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -199,7 +199,21 @@ if(isset($_POST['checkBoxArray'])) {
                             echo "<td>{$post_status}</td>";
                             echo "<td><img src='../images/{$post_image}' alt='image' class='img-responsive'></td>";
                             echo "<td>{$post_tags}</td>";
-                            echo "<td>{$post_comment_count}</td>";
+
+                            $query = "SELECT * FROM comments WHERE comment_post_id = {$post_id}";
+                            $send_comment_query = mysqli_query($connection, $query);
+
+                            $row = mysqli_fetch_array($send_comment_query);
+                            // $comment_id = $row['comment_id'];
+                            $count_comments = mysqli_num_rows($send_comment_query);
+
+                            if($count_comments>0){
+                                $comment_post_id = $row['comment_post_id'];
+                                echo "<td><a href='comment.php?id={$comment_post_id}'>{$count_comments}</a></td>";
+                            } else {
+                                echo "<td>{$count_comments}</a></td>";
+                            }
+
                             echo "<td>{$post_date}</td>";
                             echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
                             echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
